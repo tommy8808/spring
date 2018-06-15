d7f1105e98ed6435177eec8223b88f3987ef9539
User <User@219.250.236.218>
2018-06-16 03:50:50 +0900

    db 설정 및 구조 추가

--- study/src/main/java/net/study/web/HomeController.java
+++ study/src/main/java/net/study/web/HomeController.java
@@ -1,12 +1,18 @@
 package net.study.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping(value="/tt")
-	public String home(){
+
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/tt")
+	public String home() {
+		log.info("logback setting test");
 		return "index";
 	}
 }
---
+++ study/src/main/resources/ddl.sql
@@ -0,0 +1,10 @@
+
+create table spring4fs.USERS (
+	userId varchar(12) NOT NULL PRIMARY KEY,
+	password varchar(12) NOT NULL,
+	name varchar(100) NOT NULL,
+	email varchar(50),
+	
+)engine=InnoDB character set = utf8;
+
+insert into USERS values ('test1', 'password', 'test', 'test@test.com');
